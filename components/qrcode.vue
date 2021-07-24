dc4e3bd22cc66bd6270c7b75e15681b590b2ba62
liuxin04 <2597338006@qq.com>
2021-07-24 15:47:45 +0800

    first

---
+++ components/qrcode.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div ref="qrCodeUrl" class="qrcode"></div>
+    </div>
+</template>
+<script>
+export default {
+    mounted() {
+        this.creatQrCode();
+    },
+    methods: {
+        creatQrCode() {
+            var qrcode = new this.Qcode(this.$refs.qrCodeUrl, {
+                text: "xxxx", // 需要转换为二维码的内容
+                width: 100,
+                height: 100,
+                colorDark: "#000000",
+                colorLight: "#ffffff",
+                correctLevel: this.Qcode.CorrectLevel.H
+            });
+        }
+    }
+};
+</script>
+<style></style>
---
+++ plugins/qrcode.js
@@ -0,0 +1,4 @@
+import Vue from "vue"
+import Qcode from "qrcodejs2"
+Vue.prototype.Qcode = Qcode
+new Vue()
